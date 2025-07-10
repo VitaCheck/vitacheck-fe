b946e0e202c941c7d4253f4cd6a65d3743df3fd1
heeone1 <rinakim1019@gmail.com>
2025-07-11 06:31:46 +0900

    feat/#12: 마이페이지 UI 생성

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,16 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/NavBar";
 
 const RootLayout = () => {
+  const location = useLocation();
+  const hideNavbar = location.pathname.startsWith("/mypage");
   return (
     <div className="font-[Pretendard] h-full flex flex-col">
-      <header className="fixed top-0 left-0 w-full z-50 bg-white shadow-sm">
-        <div className="w-[80%] md:w-[90%] lg:w-[80%] mx-auto">
-          <Navbar />
-        </div>
-      </header>
+      {!hideNavbar && (
+        <header className="fixed top-0 left-0 w-full z-50 bg-white shadow-sm">
+          <div className="w-[80%] md:w-[90%] lg:w-[80%] mx-auto">
+            <Navbar />
+          </div>
+        </header>
+      )}
 
-      <main className="flex-1 pt-48 md:pt-25">
+      <main className={`${hideNavbar ? "" : "pt-48 md:pt-25"} flex-1`}>
         <Outlet />
       </main>
     </div>
