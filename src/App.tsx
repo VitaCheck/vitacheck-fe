fa59077e65a1439497e0f2989b9053e216e30bb5
behindacat <yulmooop@gmail.com>
2025-08-10 19:41:51 +0900

    app.tsx 수정 및 불필요한 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,10 @@ import EditProfilePage from "./pages/EditProfilePage";
 import IngredientDetailPage from "./pages/ingredients/IngredientDetailPage";
 import NoSearchResult from "./components/ingredient/NoSearchResult";
 import IngredientSearchSection from "./components/ingredient/IngredientSearchSection";
-import SocialLogin from "./components/auth/SocialLogin";
+import SocialLogin from "./components/Auth/SocialLogin";
+import SocialCallback from "./pages/auth/SocialCallback";
+import SocialSignupForm from "./pages/auth/SocialSignupForm";
+
 import EmailLoginPage from "./pages/auth/EmailLoginPage"; //자체 로그인 페이지
 import EmailSignupPage from "./pages/auth/EmailSignupPage"; //자체 회원가입 페이지
 import EmailSignupDetailPage from "./pages/auth/EmailSignupDetailPAge"; //자체 회원가입 상세 페이지
@@ -170,19 +173,11 @@ const router = createBrowserRouter([
         element: <SocialLogin />,
       },
       {
-        path: "v1/auth/google/callback",
+        path: "/auth/:provider/callback", // google|kakao|naver
         element: <SocialCallback />,
       },
       {
-        path: "v1/auth/kakao/callback",
-        element: <SocialCallback />,
-      },
-      {
-        path: "v1/auth/naver/callback",
-        element: <SocialCallback />,
-      },
-      {
-        path: "social-signup",
+        path: "/auth/social-signup",
         element: <SocialSignupForm />,
       },
 
