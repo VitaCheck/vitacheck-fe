3b2be9e4b586ff974e681e4d1baf02df507de0d2
heeone1 <rinakim1019@gmail.com>
2025-08-15 18:29:02 +0900

    fix: 반응형 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
         <div className="relative w-full flex justify-center">
           {/* 가운데 개인정보처리방침 */}
           <a
-            href="https://www.notion.so/233d3807f226800d82c4f689e39dc554?source=copy_link"
+            href="https://radical-weeder-fbc.notion.site/233d3807f226800d82c4f689e39dc554?source=copy_link"
             className="mx-4 hover:underline"
           >
             개인정보처리방침
@@ -26,7 +26,7 @@ const Footer = () => {
 
           {/* 좌측 */}
           <a
-            href="#"
+            href="https://radical-weeder-fbc.notion.site/233d3807f22680d8a2c3dfd45e504b6f?source=copy_link"
             className="absolute left-1/2 -translate-x-[160px] hover:underline"
           >
             이용약관
@@ -34,7 +34,7 @@ const Footer = () => {
 
           {/* 우측 */}
           <a
-            href="https://instagram.com"
+            href="https://www.instagram.com/vitacheck.official/"
             target="_blank"
             rel="noopener noreferrer"
             className="absolute left-1/2 translate-x-[110px] hover:underline"
--- src/components/MainPage/MainTop.tsx
+++ src/components/MainPage/MainTop.tsx
@@ -3,7 +3,16 @@ import Character from "../../assets/VitaCheckcharacter.svg";
 
 const MainTop = () => {
   return (
-    <header className="bg-gradient-to-r from-[#FFEA8C] to-[#FCFFEA] via-[#FFE88D] px-[9%] sm:px-[15%] flex flex-row sm:flex-row justify-between sm:items-center py-5">
+    <header
+      className="
+        relative z-0
+        bg-[linear-gradient(90deg,_#FFEA8C_0%,_#FFE88D_65%,_#FEF3B4_92%)]
+        sm:bg-[linear-gradient(90deg,_#FFEA8C_0%,_#FFE88D_55%,_#FCFFEA_100%)]
+        px-[9%] sm:px-[15%]
+        flex flex-row sm:flex-row justify-between sm:items-center py-5
+        [background-repeat:no-repeat]
+      "
+    >
       <div className="flex flex-col sm:items-center mb-4 sm:mb-0 items-start sm:text-center">
         <h1 className="text-2xl sm:text-4xl font-semibold text-gray-800 mt-2 sm:text-center">
           건강한 선택을 돕는
@@ -14,11 +23,12 @@ const MainTop = () => {
           className="w-3/4 sm:w-full h-auto sm:mt-5 mt-2"
         />
       </div>
-      <div className="flex justify-center sm:justify-end">
+
+      <div className="flex justify-center sm:justify-end relative z-10">
         <img
           src={Character}
           alt="VitaCheck Character"
-          className="w-[120px] sm:w-[200px] h-auto"
+          className="w-[120px] sm:w-[200px] h-auto relative z-10"
         />
       </div>
     </header>
